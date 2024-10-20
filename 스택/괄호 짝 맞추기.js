72d994ea8e7d3e804a6dcaa80e9b32946e9323da
강민혜 <alsgp9898@naver.com>
2024-10-20 23:57:01 +0900

    feat: 괄호 짝 맞추기

---
+++ 스택/괄호 짝 맞추기.js
@@ -0,0 +1,28 @@
+function solution (s) {
+    let left = 0;
+    let right = 0;
+
+    // 1. 왼쪽 괄호개수보다 오른쪽이 많아지면 false
+    // 2. 왼쪽 괄호와 오른쪽 괄호 개수가 다르면 false
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === '('){
+            left++;
+        }
+        if(s[i] === ')'){
+            right++;
+        }
+        if(left < right) {
+            return false;
+        }
+    }
+    return left !== right ? false : true
+}
+
+console.log(solution("(())()"))
+console.log(solution("((())()"))
+console.log(solution('(())())'))
+console.log(solution('(((()())()'))
+console.log(solution('(()())((()))'))
+console.log(solution('((()()(()))(((())))()'))
+console.log(solution('()()()()(()()())()'))
+console.log(solution('(()((())()('))
