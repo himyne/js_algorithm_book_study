92540de8f8c6e2dde37cb2c79f848b4130c87635
강민혜 <alsgp9898@naver.com>
2024-12-05 13:00:16 +0900

    feat: 예상 대진표

--- 트리/예상 대진표.js
+++ 트리/예상 대진표.js
@@ -1,11 +1,14 @@
 function solution(n, a, b) {
     let answer = 0;
     
+    // a와 b가 같은 번호를 받을 때까지 반복
     while(a != b) {
         a = Math.ceil(a / 2);
         b = Math.ceil(b / 2);
         answer += 1;
     }
-    
+
     return answer;
 }
+
+console.log(solution(8, 4, 7));
